1566f1d27986c513ad547eff7afa1521c2e6da04
Kate Sinitsyna <my.advertisement2017@gmail.com>
2025-08-07 23:18:56 +0300

    added SimpleJUnitTest

---
+++ src/test/java/SimpleJUnitTest.java
@@ -0,0 +1,53 @@
+package src.test.java;
+
+import org.junit.jupiter.api.*;
+
+public class SimpleJUnitTest {
+
+    int result;
+
+    @BeforeAll
+    static void beforeAll(){
+        System.out.println("###    beforeAll\n");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("###    beforeEach");
+        result = getResult();
+    }
+
+    @Test
+    void firstTest() {
+        System.out.println("###    firstTest");
+        Assertions.assertTrue(result>2);
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("###    secondTest");
+        Assertions.assertTrue(result>2);
+    }
+
+    @Test
+    void thirdTest() {
+        System.out.println("###    thirdTest");
+        Assertions.assertTrue(result>2);
+    }
+
+    private int getResult() {
+        return 3;
+    }
+
+    @AfterEach
+    void AfterEach() {
+        System.out.println("###    AfterEach\n");
+        result = 0;
+    }
+
+    @AfterAll
+    static void afterAll(){
+        System.out.println("###    AfterAll\n");
+    }
+}
+
